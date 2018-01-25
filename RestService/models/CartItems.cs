3da547190a8e88bd41201fb24a8ea217a3a72d91
Masgeek <barsamms@gmail.com>
2018-01-25 16:00:13 +0300

    updates

---
+++ RestService/models/CartItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace RestService.models
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class CartItems
+    {
+        public int CART_ITEM_ID { get; set; }
+
+        public int USER_ID { get; set; }
+
+        public int ITEM_TYPE_ID { get; set; }
+
+        public int QUANTITY { get; set; }
+
+
+        public long CART_TIMESTAMP { get; set; }
+        public double ITEM_PRICE { get; set; }
+
+        public string ITEM_SIZE { get; set; }
+
+        public DateTime CREATED_AT { get; set; }
+
+        public DateTime UPDATED_AT { get; set; }
+
+        public MenuCategoryItems MENU_CAT_ITEM { get; set; }
+    }
+}
