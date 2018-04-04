24c1f13b077ce14ce6ebad5a02add4a2af880a7f
Masgeek <barsamms@gmail.com>
2018-04-04 13:47:31 +0300

    Updates on views

---
+++ LoginBestPractice.iOS/LoginPageViewController.designer.cs
@@ -0,0 +1,60 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace LoginBestPractice.iOS
+{
+    [Register ("LoginPageViewController")]
+    partial class LoginPageViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PasswordTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SignUpButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField UserNameTextView { get; set; }
+
+        [Action ("LoginButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void LoginButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LoginButton != null) {
+                LoginButton.Dispose ();
+                LoginButton = null;
+            }
+
+            if (PasswordTextView != null) {
+                PasswordTextView.Dispose ();
+                PasswordTextView = null;
+            }
+
+            if (SignUpButton != null) {
+                SignUpButton.Dispose ();
+                SignUpButton = null;
+            }
+
+            if (UserNameTextView != null) {
+                UserNameTextView.Dispose ();
+                UserNameTextView = null;
+            }
+        }
+    }
+}
---
+++ LoginBestPractice.iOS/SignUpViewController.designer.cs
@@ -0,0 +1,73 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace LoginBestPractice.iOS
+{
+    [Register ("SignUpViewController")]
+    partial class SignUpViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField ConfirmPasswordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PasswordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SignUpButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField UserNameTextView { get; set; }
+
+        [Action ("CancelButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void CancelButton_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("SignUpButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void SignUpButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (CancelButton != null) {
+                CancelButton.Dispose ();
+                CancelButton = null;
+            }
+
+            if (ConfirmPasswordTextField != null) {
+                ConfirmPasswordTextField.Dispose ();
+                ConfirmPasswordTextField = null;
+            }
+
+            if (PasswordTextField != null) {
+                PasswordTextField.Dispose ();
+                PasswordTextField = null;
+            }
+
+            if (SignUpButton != null) {
+                SignUpButton.Dispose ();
+                SignUpButton = null;
+            }
+
+            if (UserNameTextView != null) {
+                UserNameTextView.Dispose ();
+                UserNameTextView = null;
+            }
+        }
+    }
+}
