17b8d01c932d443ae43c55a9a399b2dd2d169f60
Sammy Barasa <barsamms@gmail.com>
2018-05-14 15:24:48 +0300

    Updates on repository
    Added app icons

--- PizzaOut.IOS/LoginPageViewController.designer.cs
+++ PizzaOut.IOS/LoginPageViewController.designer.cs
@@ -1,12 +1,12 @@
-﻿// WARNING
+// WARNING
 //
 // This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
-
-using System.CodeDom.Compiler;
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace PizzaOut.IOS
 {
