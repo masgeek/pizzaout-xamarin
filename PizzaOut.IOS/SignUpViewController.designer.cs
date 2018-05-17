f5bb36e98272c1cd937999869c74ebc17a1793b4
Sammy Barasa <barsamms@gmail.com>
2018-05-17 18:22:07 +0300

    Storyboard Updates Added names to the ui elements
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- PizzaOut.IOS/SignUpViewController.designer.cs
+++ PizzaOut.IOS/SignUpViewController.designer.cs
@@ -1,4 +1,4 @@
-// WARNING
+﻿// WARNING
 //
 // This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
@@ -13,8 +13,88 @@ namespace PizzaOut.IOS
     [Register ("SignUpViewController")]
     partial class SignUpViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField ConfirmPasswordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField EmailTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField OtherNamesTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PasswordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PhoneTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SignUpButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField SurNameTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField UserNameTextView { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (CancelButton != null) {
+                CancelButton.Dispose ();
+                CancelButton = null;
+            }
+
+            if (ConfirmPasswordTextField != null) {
+                ConfirmPasswordTextField.Dispose ();
+                ConfirmPasswordTextField = null;
+            }
+
+            if (EmailTextView != null) {
+                EmailTextView.Dispose ();
+                EmailTextView = null;
+            }
+
+            if (OtherNamesTextView != null) {
+                OtherNamesTextView.Dispose ();
+                OtherNamesTextView = null;
+            }
+
+            if (PasswordTextField != null) {
+                PasswordTextField.Dispose ();
+                PasswordTextField = null;
+            }
+
+            if (PhoneTextView != null) {
+                PhoneTextView.Dispose ();
+                PhoneTextView = null;
+            }
+
+            if (SignUpButton != null) {
+                SignUpButton.Dispose ();
+                SignUpButton = null;
+            }
+
+            if (SurNameTextView != null) {
+                SurNameTextView.Dispose ();
+                SurNameTextView = null;
+            }
+
+            if (UserNameTextView != null) {
+                UserNameTextView.Dispose ();
+                UserNameTextView = null;
+            }
         }
     }
 }
