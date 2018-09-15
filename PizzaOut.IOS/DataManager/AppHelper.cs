75fdfa2957b915396d33b6645355ce8e083c59da
Sammy Barasa <barsamms@gmail.com>
2018-09-15 11:01:56 +0300

    Added better checking of internet connection and network availability Updated method comments

--- PizzaOut.IOS/DataManager/AppHelper.cs
+++ PizzaOut.IOS/DataManager/AppHelper.cs
@@ -11,15 +11,28 @@ namespace PizzaOut.IOS.DataManager
 {
     public class AppHelper
     {
+        /// <summary>
+        /// Do we have internet connection.?
+        /// </summary>
+        /// <returns><c>true</c>, if internet connection is there, <c>false</c> otherwise.</returns>
         public static bool HasInternetConnection()
         {
             return CrossConnectivity.Current.IsConnected;
         }
 
+        /// <summary>
+        /// Is the network available.
+        /// </summary>
+        /// <returns><c>true</c>, if network is available, <c>false</c> otherwise.</returns>
+        /// <param name="testHost">Test host.</param>
         public static bool IsNetworkAvailable(string testHost = "http://google.com")
         {
             //Via Reachability  
-            return Reachability.IsHostReachable(testHost);
+            if (HasInternetConnection())
+            {
+                return Reachability.IsHostReachable(testHost);
+            }
+            return false;
         }
     }
 }
